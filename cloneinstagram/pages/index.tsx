d37028befefa141ccce173709381fc6475e63600
Gustavo Fernandes Costa <84602100+gustavofcosta@users.noreply.github.com>
2022-06-21 16:13:05 -0300

    config initial and build navbar

---
+++ cloneinstagram/pages/index.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Header from '../components/Header'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Instagram 2.0</title>
+        <link rel="icon" href="https://links.papareact.com/jjm" />
+      </Head >
+
+      <Header />
+
+      {/* Feed */}
+
+      {/* Modal */}
+
+
+    </div >
+  )
+}
+
+export default Home
